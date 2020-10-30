2179359584efa9dfcbf5bd89e9ec0c4dd1113cf4
DanJGV <dangv93@gmail.com>
2020-10-30 09:50:50 -0500

    OnlineMP

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CameraFollow : MonoBehaviour
+{
+	
+	Transform target;
+	public float smoothing = 5f;
+	public bool start = false;
+	GameObject[] players;
+	
+	
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		
+		
+	}
+
+	void FixedUpdate()
+	{
+		if (start == false)
+		{
+		
+			players = GameObject.FindGameObjectsWithTag("Player");
+
+			for(int i = 0; i < players.Length; i++)
+            {
+				if (players[i].GetComponent<PlayerMovement>().isLocalPlayer)
+                {
+					target = players[i].transform;
+					offset = transform.position - target.position;
+					start = true;
+				}
+            }
+			
+
+		}
+
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ SurvivalShooter/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using Mirror;
+public class EnemyManager : NetworkBehaviour
+{
+   
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    GameObject[] players;
+    PlayerHealth playerHealth;
+    bool start = false;
+
+    void Start ()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+    }
+
+    private void Update()
+    {
+        if (start == false)
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].GetComponent<PlayerMovement>().isLocalPlayer)
+                {
+                    playerHealth = players[i].GetComponent<PlayerHealth>();
+                    start = true;
+                }
+            }
+        }
+    }
+
+
+    void Spawn ()
+    {
+        /*if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);*/
+        RpcSpawn();
+    }
+
+    [ClientRpc]
+
+    void RpcSpawn()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        GameObject e = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        NetworkServer.Spawn(e);
+    }
+}
